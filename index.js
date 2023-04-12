62f4e6a5a0516b2125131e30c96f040d872d72fb
roesnera <adam.roesner@generation.org>
2023-04-12 15:33:53 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import quotes from "./dummyAPI.js";
+
+// you can access individual quotes from the quotes array like this:
+console.log(quotes[0].text); // outputs the text of the quote
+console.log(quotes[0].author); // outputs the author of the quote
+
+
+// your job is to use DOM manipulation to display a random quote in the index.html page that is retrieved from the quotes array
