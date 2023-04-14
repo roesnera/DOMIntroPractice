5fce96dd593e43ac878b8f2d840837b51e87fd3d
Muhiddin Kurbonov <muhiddindc@gmail.com>
2023-04-14 17:44:15 -0500

    add copy quote functionality with fadeout message

--- index.js
+++ index.js
@@ -20,6 +20,39 @@ function getRandomQuote() {
   const randomIdx = random();
   quoteText.innerText = quotes[randomIdx].text;
   quoteAuthor.innerText = quotes[randomIdx].author;
+  showCopy();
 }
 
+function showCopy() {
+  document.getElementById("btn-copy").style.visibility = "visible";
+}
+
+function fadeOut(el) {
+  let intervalId = setInterval(function () {
+    var opacity = el.style.opacity;
+    if (opacity > 0) {
+      opacity -= 0.1;
+      el.style.opacity = opacity;
+    } else {
+      clearInterval(intervalId); // Stop the interval when opacity reaches 0
+      el.style.display = "none"; // Hide the element
+    }
+  }, 100);
+}
+
+const handleCopy = async () => {
+  const quoteText = document.querySelector(".quote").innerText;
+  let copied = document.getElementById("copied");
+  copied.style.opacity = 1;
+  copied.style.display = "block";
+  try {
+    await navigator.clipboard.writeText(quoteText);
+    copied.innerText = "Copied to clipboard!";
+    fadeOut(copied);
+  } catch (error) {
+    copied.innerText = "Something went wrong!";
+  }
+};
+
 btnRandom.addEventListener("click", getRandomQuote);
+btnCopy.addEventListener("click", handleCopy);
