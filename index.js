ae7713a894fa88441c3e5ae3e0eb59993ee16cbc
Muhiddin Kurbonov <muhiddindc@gmail.com>
2023-04-14 17:40:45 -0500

    implement get random quotes functionality and event handling

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ const inputSearch = document.getElementById("input-search");
 const error = document.getElementById("error");
 const btnSearch = document.getElementById("btn-search");
 const quoteSearch = document.getElementsByClassName("quote-search")[0];
+
+// get random quote index
+function random() {
+  return Math.floor(Math.random(0) * quotes.length);
+}
+
+function getRandomQuote() {
+  const randomIdx = random();
+  quoteText.innerText = quotes[randomIdx].text;
+  quoteAuthor.innerText = quotes[randomIdx].author;
+}
+
+btnRandom.addEventListener("click", getRandomQuote);
